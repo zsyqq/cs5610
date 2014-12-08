646f1b1132442751900cbfa8766d2b04a26ac328
zsyqq <zsyqq@ccs.neu.edu>
2014-12-08 04:09:10 -0500

    replace local image

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ var path = require('path')
 var mongoose = require('mongoose')
 var mongoStore = require('connect-mongo')(express)
 // for local host
-// var port = process.env.PORT || 8000
+var port = process.env.PORT || 8000
 // for openshift server side
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8000
+// var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"
+// var port = process.env.OPENSHIFT_NODEJS_PORT || 8000
 var app = express()
 var fs = require('fs')
-// for local 
-// var dbUrl = 'mongodb://localhost/webdb'
+// for local  
+var dbUrl = 'mongodb://localhost/webdb'
 // for openshift server side
-var dbUrl = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    process.env.OPENSHIFT_APP_NAME;
+// var dbUrl = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
+//     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+//     process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+//     process.env.OPENSHIFT_APP_NAME;
 mongoose.connect(dbUrl)
 
 
@@ -62,8 +62,8 @@ if ('development' === app.get('env')) {
 
 require('./config/routes')(app)
 
-app.listen(port, ipaddress)
-// app.listen(port)
+// app.listen(port, ipaddress)
+app.listen(port)
 app.locals.moment = require('moment')
 app.use(express.static(path.join(__dirname, 'public')))
 
